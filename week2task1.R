58eec3995d47bdafccac0be3f9844dcc7fbda860
TeodorDyakov <tedy1996@gmail.com>
2020-10-15 02:28:49 +0300

    add solutions

--- week2task1.R
+++ week2task1.R
@@ -1,6 +1,9 @@
+library(MASS)
+
 #намираме първо всички стойности в колоната Smoke
 unique(survey$Smoke)
 
+#Задача 1
 #вероятноста случайно избран човек да е редовен пушач
 sum(survey$Smoke == 'Regul', na.rm = TRUE)/nrow(survey)
 
@@ -19,5 +22,16 @@ regulOrHeavyCount = sum(survey$Smoke %in% c('Regul', 'Heavy'), na.rm = TRUE)
 #вероятноста случайно избран редовен пушач да е мъж
 sum(survey$Smoke %in% c("Regul", "Heavy") & survey$Sex == 'Male', na.rm = TRUE)/regulOrHeavyCount
 
+#Зад 2
 #барплот на броя на типовете пушачи
 barplot(table(survey$Smoke))
+
+#Зад 4
+hist(survey$Height)
+
+#Зад 5
+#хистограма на пулса на студентите
+hist(survey$Pulse)
+
+#графика плътността на разпределението на пулса 
+plot(density(survey$Pulse, na.rm = TRUE))
