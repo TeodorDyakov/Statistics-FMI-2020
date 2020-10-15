fd7e373865a71822f42eef76e3f92b78e0283436
TeodorDyakov <tedy1996@gmail.com>
2020-10-16 00:36:20 +0300

    cleanup

--- week2tasks.R
+++ week2tasks.R
@@ -1,15 +1,3 @@
----
-title: "Week 2"
-author: "Teodor"
-date: "10/15/2020"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-```{r week 2}
 library(MASS)
 
 #намираме първо всички стойности в колоната Smoke
@@ -17,26 +5,49 @@ unique(survey$Smoke)
 
 #Задача 1
 #вероятноста случайно избран човек да е редовен пушач
-sum(survey$Smoke == 'Regul' | survey$Smoke == 'Heavy', na.rm = TRUE)/nrow(survey)
+sum(survey$Smoke == 'Regul', na.rm = TRUE)/nrow(survey)
+
+maleSmokers = sum(survey$Smoke %in% c("Regul", "Heavy") & survey$Sex == 'Male', na.rm = TRUE)
 
 #вероятността случайно ибзбран човек да е мъж и редовен пушач
-sum(survey$Smoke %in% c("Regul", "Heavy") & survey$Sex == 'Male', na.rm = TRUE)/nrow(survey)
+maleSmokers/nrow(survey)
 
 #броят на всички мъже
 maleNumber = sum(survey$Sex == 'Male', na.rm = TRUE)
 
 #вероятността случайно избран мъж да е редовен пушач
-sum(survey$Sex == 'Male' & survey$Smoke %in% c("Regul", "Heavy"), na.rm = TRUE)/maleNumber
+prob = maleSmokers/maleNumber
 
 #броят на всички редовни пушачи
 regulOrHeavyCount = sum(survey$Smoke %in% c('Regul', 'Heavy'), na.rm = TRUE)
 
 #вероятноста случайно избран редовен пушач да е мъж
-sum(survey$Smoke %in% c("Regul", "Heavy") & survey$Sex == 'Male', na.rm = TRUE)/regulOrHeavyCount
+maleSmokers/regulOrHeavyCount
 
 #Зад 2
 #барплот на броя на типовете пушачи
 barplot(table(survey$Smoke))
+
+#пушене в зависимост от пола
+barplot(c(maleSmokers, maleNumber - maleSmokers), names.arg = c('male smokers', 'male non-smokers'))
+
+#Зад 3
+#некви статистики
+summary(survey$Height)
+heightMean = mean(survey$Height, na.rm = TRUE)
+heightSd = sd(survey$Height, na.rm = TRUE)
+
+
+maleHeights = survey[survey$Sex == 'Male', 'Height']
+summary(maleHeights)
+sd(maleHeights, na.rm = TRUE)
+
+femaleHeights = survey[survey$Sex == 'Female', 'Height']
+summary(femaleHeights)
+sd(femaleHeights, na.rm = TRUE)
+
+#каква част от студентите се различават от ср. височина с <= 1 стандартно отклонение?
+sum(abs(survey$Height - heightMean) <= heightSd, na.rm = TRUE)/sum(!is.na(survey$Height))
 
 #Зад 4
 hist(survey$Height, breaks = 20)
@@ -47,4 +58,4 @@ hist(survey$Pulse)
 
 #графика плътността на разпределението на пулса 
 plot(density(survey$Pulse, na.rm = TRUE))
-```
+
